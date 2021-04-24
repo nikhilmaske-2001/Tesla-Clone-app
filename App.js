097baae6bc05b88e05789cd920a4d30238d78c94
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-24 13:48:13 +0530

    Bg-Image: Add a background image.

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
       <View style={styles.carContainer}>
+        <ImageBackground source={require('./assets/images/ModelS.jpeg')} style={styles.image} />
 
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
@@ -46,4 +48,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#5c5e62'
   },
+
+  image: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'cover',
+    position: 'absolute'
+  }
 });
